5e5ca2e26ea4c335138e59d5c355948a445788b4
daniaalnahdi <alnahdi.d@husky.neu.edu>
2020-10-08 03:34:28 +0300

    configure webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { NavStates, NavLinks, Transitions } from './js/modules.js';
+import './scss/main.scss'
+
+(function () {
+  NavStates.init();
+  NavLinks.init();
+  Transitions.init();
+})();
