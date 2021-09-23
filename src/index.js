aaf6ced35afad462238b4ec8d9ec40ef52dfc5ec
daniaalnahdi <alnahdi.d@husky.neu.edu>
2021-09-22 22:45:33 -0400

    update sass

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { FontAwesome } from './js/vendors';
 import './assets/images/favicon.png';
 import './scss/main.scss';
 
-(() => {
+const app = async () => {
   FontAwesome.init();
   Translator.init();
   NavStates.init();
   NavLinks.init();
   Transitions.init();
-})();
+};
+
+document.addEventListener('DOMContentLoaded', app);
